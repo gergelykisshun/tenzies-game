f2bf5a701667844433e5d7bae37d36667c98ed0b
GergelyKiss <gergely.kiss.hun@gmail.com>
2022-04-26 21:49:29 +0200

    basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Die from './components/Die/Die';
 
 const App = () => {
+  const allNewDice = () => {
+    return new Array(10).fill(0).map(num => Math.ceil(Math.random() * 6));
+  };
+
+  const [diceArr, setDiceArr] = useState(allNewDice());
+
+  const rollNewDice = () => {
+    setDiceArr(allNewDice());
+  };
 
   return (
     <main className='section-container'>
       <div className='game-container'>
         <div className='game-area'>
+          <h1>
+            Tenzies
+            <p>
+              Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+            </p>
+          </h1>
           <div className='dice-container'>
-            {new Array(10).fill('0').map(die => <Die value={Math.ceil(Math.random() * 6)}/>)}
+            {diceArr.map(die => <Die value={die}/>)}
           </div>
+          <button className='roll-btn' onClick={rollNewDice}>Roll</button>
         </div>
       </div>
     </main>
